19f5b7c313a1126458cc4b482213b2b64533f32e
Adam <adamskubel@gmail.com>
2011-12-27 19:34:48 -0500

    Automating build

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,19 +4,8 @@ include $(CLEAR_VARS)
 
 OPENCV_CAMERA_MODULES:=off
 
-OPENCV_MK_PATH= ../OpenCV/OpenCV-2.3.1/share/OpenCV/OpenCV.mk
+OPENCV_MK_PATH= ${OPENCV_HOME}/OpenCV-2.3.1/share/OpenCV/OpenCV.mk
 include $(OPENCV_MK_PATH)
-
-
-#include ../OpenCV/includeOpenCV.mk
-#OPENCV_MK_PATH = 
-#ifeq ("$(wildcard $(OPENCV_MK_PATH))","")
-#	#try to load OpenCV.mk from default install location
-#	include $(TOOLCHAIN_PREBUILT_ROOT)/user/share/OpenCV/OpenCV.mk
-#else
-#	include $(OPENCV_MK_PATH)
-#endif
-
 
 
 LOCAL_MODULE    := amplify_reality
